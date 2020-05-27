9f7c967db8cb433fb747d7f46b5070e32aa57b3c
rolim8 <63255314+rolim8@users.noreply.github.com>
2020-05-27 20:26:42 -0300

    Add files via upload

---
+++ JogoDaVelha/src/JogoVelha/JogoVelha.java
@@ -0,0 +1,12 @@
+package JogoVelha;
+
+/**
+ *
+ * @author rolim
+ */
+public class JogoVelha {
+    public static void main(String[] args) {
+        Jogo jogo = new Jogo();  
+        
+    }   
+}
---
+++ JogoDaVelha/src/JogoVelha/NPC.java
@@ -0,0 +1,21 @@
+package JogoVelha;
+
+/**
+ *
+ * @author rolim
+ */
+public class NPC extends Jogador{
+    public NPC(int jogador) {
+        super(jogador);
+    }
+
+    @Override
+    public void jogar(Tabuleiro tabuleiro){
+        
+    }
+    
+    @Override
+    public void Tentativa(Tabuleiro tabuleiro){
+        
+    }
+}
